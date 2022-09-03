77036279d280e48378729da2d42461720a5031b8
sadia-mehjabin <jhidnicraft@gmail.com>
2022-09-03 18:28:01 +0600

    news section added

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const displayCatagories = (data) =>{
         div.classList.add('m-4');
         div.classList.add('text-decoration-none');
         div.innerHTML = `
-        <a href="">${item.category_name}</a> 
+        <a class="text-decoration-none" href="">${item.category_name}</a> 
         `
         catagory.appendChild(div); 
     })
@@ -20,4 +20,22 @@ const displayCatagories = (data) =>{
 // news url 
 fetch('https://openapi.programming-hero.com/api/news/category/01')
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => displayNews(data))
+
+const displayNews = (data, catagory_id) => {
+    const container = document.getElementById('container');
+    const getNews = data.data;
+    getNews.forEach(news => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+        <img src=${news.image_url} class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${news.title}</h5>
+            <p class="card-text">${news.details}</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        `
+        container.appendChild(div);
+        console.log(news)
+    })
+}
