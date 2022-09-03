63ee60c0c268494dcf11b02d8826f59c0c476172
sadia-mehjabin <jhidnicraft@gmail.com>
2022-09-03 23:36:54 +0600

    dynamic url added

--- script.js
+++ script.js
@@ -9,20 +9,24 @@ const displayCatagories = (data) =>{
     getArray.forEach(item => {
         const div = document.createElement('div');
         div.classList.add('m-4');
-        div.classList.add('text-decoration-none');
         div.innerHTML = `
-        <a class="text-decoration-none" href="">${item.category_name}</a> 
+        <button class="btn btn-light" onclick="console.log(${item.category_id})">${item.category_name}</button> 
         `
         catagory.appendChild(div); 
+        fetch(`https://openapi.programming-hero.com/api/news/category/${item.category_id}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+
+        
     })
 }
 
 // news url 
-fetch('https://openapi.programming-hero.com/api/news/category/01')
+fetch(`https://openapi.programming-hero.com/api/news/category/08`)
 .then(res => res.json())
 .then(data => displayNews(data))
 
-const displayNews = (data, catagory_id) => {
+const displayNews = (data) => {
     const container = document.getElementById('container');
     const getNews = data.data;
     getNews.forEach(news => {
@@ -32,10 +36,21 @@ const displayNews = (data, catagory_id) => {
         <img src=${news.image_url} class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">${news.title}</h5>
-            <p class="card-text">${news.details}</p>
+            <p class="card-text">${news.details.slice(0, 500)}.....</p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div class="d-flex">
+            <img src="${news.author.img}" class="w-25 h-25 rounded-5" alt="">
+            <p>${news.author.name}</p>
+          </div>
         `
         container.appendChild(div);
-        console.log(news)
+        
     })
 }
+// const category = (id) => {
+//     fetch(`https://openapi.programming-hero.com/api/news/category/${data}`)
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+    
+    
+// }
